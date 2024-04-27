6e5b228997232039524bc90d163a027af0eba75e
nagymathev <nagymathev@gmail.com>
2024-04-28 00:51:34 +0200

    flake init

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+
+description = "NixOs flake for my Stellaris laptop";
+
+inputs = {
+	nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+};
+
+outputs = { self, nixpkgs, ... }@inputs: {
+	nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
+		system = "x86_64-linux";
+		modules = [
+			./configuration.nix
+		];
+	};
+};
+
+}
