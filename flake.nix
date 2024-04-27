fa9eb2caa17b0d95673e3a99ded78a444ea77671
nagymathev <nagymathev@gmail.com>
2024-04-28 01:34:52 +0200

    nixos hardware for a similar laptop with the same CPU

--- flake.nix
+++ flake.nix
@@ -4,13 +4,15 @@ description = "NixOs flake for my Stellaris laptop";
 
 inputs = {
 	nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+	nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 };
 
-outputs = { self, nixpkgs, ... }@inputs: {
+outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
 	nixosConfigurations.stellaris = nixpkgs.lib.nixosSystem {
 		system = "x86_64-linux";
 		modules = [
 			./configuration.nix
+			nixos-hardware.nixosModules.tuxedo-pulse-14-gen3
 		];
 	};
 };
