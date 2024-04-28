cd01f74468f930ab89ece9499c915494b0d77a37
nagymathev <nagymathev@gmail.com>
2024-04-28 02:47:02 +0200

    basic home-manager

--- flake.nix
+++ flake.nix
@@ -4,7 +4,12 @@ description = "NixOs flake for my Stellaris laptop";
 
 inputs = {
 	nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+	nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 	nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+	home-manager = {
+		url = "github:nix-community/home-manager/release-23.11";
+		inputs.nixpkgs.follows = "nixpkgs";
+	};
 };
 
 outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
@@ -13,6 +18,12 @@ outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
 		modules = [
 			./configuration.nix
 			nixos-hardware.nixosModules.tuxedo-pulse-14-gen3
+
+			home-manager.nixosModules.home-manager {
+				home-manager.useGlobalPkgs = true;
+				home-manager.useUserPackages = true;
+				home-manager.users.viktor = import ./home.nix;
+			}
 		];
 	};
 };
